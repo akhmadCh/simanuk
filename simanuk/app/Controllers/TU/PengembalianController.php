6840efbec081ad0feee45d7a36271eb2015f8699
Chaidar <chaidarmerak1996@gmail.com>
2025-12-06 13:06:41 +0800

    refactor sedikit

--- simanuk/app/Controllers/TU/PengembalianController.php
+++ simanuk/app/Controllers/TU/PengembalianController.php
@@ -244,7 +244,7 @@ class PengembalianController extends BaseController
                 return redirect()->back()->with('error', 'Gagal memproses transaksi.');
             }
 
-            return redirect()->to(site_url('admin/pengembalian'))->with('message', 'Pengembalian selesai. Stok barang BAIK telah dikembalikan, barang RUSAK tercatat di laporan.');
+            return redirect()->to(site_url('admin/pengembalian'))->with('message', 'Pengembalian selesai. Stok barang BAIK telah dikembalikan, dan jika ada barang RUSAK akan tercatat di laporan.');
         } catch (\Exception $e) {
             $db->transRollback();
             return redirect()->back()->with('error', $e->getMessage());
